4abfc365820daf48b66f27056b88e15f7644b553
Noam <nmbarel@gmail.com>
2024-04-23 15:08:00 +0300

    added GEMINI support

--- replacer.py
+++ replacer.py
@@ -12,4 +12,10 @@ def request(flow: http.HTTPFlow) -> None:
         if "messages" in d:
             d["messages"][0]["content"]["parts"] = prompt.split(" ")
             flow.request.content = json.dumps(d).encode()
-    
+    if flow.request.host == "gemini.google.com" and flow.request.urlencoded_form["f.req"]:
+        ctx.log.info(flow.request.urlencoded_form["f.req"])
+        requestContent = flow.request.urlencoded_form["f.req"]
+        promptstartIndex = 11 # gemini messages always start with 11 chars of filler before the prompt is written
+        promptendIndex = promptstartIndex + requestContent[promptstartIndex:].find("\\\",0,null,null,null,null") # this always comes after the prompt
+        newContent = requestContent[:promptstartIndex] + prompt + requestContent[promptendIndex:]
+        flow.request.urlencoded_form["f.req"] = newContent
